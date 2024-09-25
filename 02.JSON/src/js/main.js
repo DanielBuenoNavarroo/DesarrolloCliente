f610a87cbf1b48c4899096248d983bff5dedc482
Daniel Bueno Navarro <danielbuenonavarro@gmail.com>
2024-09-25 21:03:24 +0200

    Cambios

--- 02.JSON/src/js/main.js
+++ 02.JSON/src/js/main.js
@@ -1,13 +1,7 @@
 const handleSubmit = (event) => {
   let isValidated = false;
   event.preventDefault();
-  for (let i = 0; i < event.target.length - 1; i++) {
-    const item = event.target[i]
-    console.log(item)
-    if (!event.target[i]) {
-      console.log("Falta el: ", event.target[i].name);
-    }
-  }
+  
   return isValidated;
 };
 
